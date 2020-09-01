a3814bb0921cdf7b3499599dd813266892af8baa
jinsu jang <jinsyu.com@gmail.com>
2020-09-02 02:26:59 +0900

    Initial Commit

---
+++ clieneilc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  clieneilc
+//
+//  Created by jinsyu on 2020/09/02.
+//  Copyright © 2020 jinsyu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
